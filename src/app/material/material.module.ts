74ce2202a08f52216989418ced57772aaaf752db
jonnieraptor <78690448+jonnieraptor@users.noreply.github.com>
2023-05-10 15:08:53 -0500

    3 files modified

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   imports: [
@@ -20,18 +20,18 @@ import { MatTableModule } from '@angular/material/table'
     MatGridListModule,
     MatInputModule,
     MatFormFieldModule,
-    MatTableModule
+    MatTableModule,
   ],
   exports: [
     MatButtonModule,
     MatMenuModule,
-    MatToolbarModule ,
+    MatToolbarModule,
     MatIconModule,
     MatCardModule,
     MatGridListModule,
     MatInputModule,
     MatFormFieldModule,
-    MatTableModule
-  ]
+    MatTableModule,
+  ],
 })
 export class MaterialModule {}
